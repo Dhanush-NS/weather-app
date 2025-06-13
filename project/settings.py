028d0efb34f40ddeda26430125438c8ef1b8505c
Dhanush-NS <nsdhanush74@gmail.com>
2025-06-13 12:00:50 +0530

    updated requiremtns.txt

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0xah(!sq*b^j4#u+_66#i%nxpk1-!*yh$7s#ljync8-9r2)rtx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
